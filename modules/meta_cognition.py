852bed1e7b84fea4e0797f42518d7ad237ad964d
t81dev <sansdisk0134@icloud.com>
2025-07-19 11:15:42 -0400

    Update meta_cognition.py

--- modules/meta_cognition.py
+++ modules/meta_cognition.py
@@ -1,1 +1,11 @@
+from utils.prompt_utils import call_gpt
+
+class MetaCognition:
+    def review(self, output):
+        prompt = f"""
+        Review the following reasoning for errors, biases, and improve it:
+        {output}
+        Provide a corrected version:
+        """
+        return call_gpt(prompt)
 
