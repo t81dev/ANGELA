068cda09f4324e3a4577758f484e96b25439d341
t81dev <sansdisk0134@icloud.com>
2025-07-19 11:24:46 -0400

    Update concept_synthesizer.py

--- modules/concept_synthesizer.py
+++ modules/concept_synthesizer.py
@@ -1,1 +1,13 @@
+from utils.prompt_utils import call_gpt
+
+class ConceptSynthesizer:
+    def synthesize(self, data):
+        prompt = f"""
+        Given the following data:
+        {data}
+
+        Synthesize a new concept or analogy that unifies these ideas. 
+        Be creative, insightful, and provide a clear explanation.
+        """
+        return call_gpt(prompt)
 
