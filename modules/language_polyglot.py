7bd5b3b12de7795342175550e23d27fd5b0a0db5
t81dev <sansdisk0134@icloud.com>
2025-07-19 11:18:02 -0400

    Update language_polyglot.py

--- modules/language_polyglot.py
+++ modules/language_polyglot.py
@@ -1,1 +1,10 @@
+from utils.prompt_utils import call_gpt
+
+class LanguagePolyglot:
+    def translate(self, text, target_language):
+        prompt = f"""
+        Translate the following text into {target_language}:
+        "{text}"
+        """
+        return call_gpt(prompt)
 
