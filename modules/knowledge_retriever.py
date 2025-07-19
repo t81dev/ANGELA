8215c57a18cc2378113757c9f2c3cd36f9592015
t81dev <sansdisk0134@icloud.com>
2025-07-19 11:25:03 -0400

    Update knowledge_retriever.py

--- modules/knowledge_retriever.py
+++ modules/knowledge_retriever.py
@@ -1,1 +1,12 @@
+from utils.prompt_utils import call_gpt
+
+class KnowledgeRetriever:
+    def retrieve(self, query):
+        prompt = f"""
+        Search for factual information or relevant background knowledge on:
+        "{query}"
+
+        Provide a concise and accurate summary.
+        """
+        return call_gpt(prompt)
 
