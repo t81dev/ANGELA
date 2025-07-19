7e63c281a496c683ecd85ce8dba8d10c414580c9
t81dev <sansdisk0134@icloud.com>
2025-07-19 11:19:14 -0400

    Update alignment_guard.py

--- modules/alignment_guard.py
+++ modules/alignment_guard.py
@@ -1,1 +1,8 @@
+class AlignmentGuard:
+    def check(self, user_input):
+        # Very basic filter: block dangerous requests
+        banned_keywords = ["hack", "virus", "destroy", "harm"]
+        if any(keyword in user_input.lower() for keyword in banned_keywords):
+            return False
+        return True
 
