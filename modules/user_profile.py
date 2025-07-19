75154122dd9d802046087fd06002d2e28dc1f6a0
t81dev <sansdisk0134@icloud.com>
2025-07-19 11:19:32 -0400

    Update user_profile.py

--- modules/user_profile.py
+++ modules/user_profile.py
@@ -1,1 +1,9 @@
+class UserProfile:
+    def __init__(self):
+        self.preferences = {"style": "neutral"}
 
+    def get_preferences(self):
+        return self.preferences
+
+    def update_preferences(self, new_prefs):
+        self.preferences.update(new_prefs)
