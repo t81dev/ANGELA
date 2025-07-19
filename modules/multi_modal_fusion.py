982e34afde0754d48a2f7c446b92a3c8d82f4dd1
t81dev <sansdisk0134@icloud.com>
2025-07-19 11:17:32 -0400

    Update multi_modal_fusion.py

--- modules/multi_modal_fusion.py
+++ modules/multi_modal_fusion.py
@@ -1,1 +1,10 @@
+from utils.prompt_utils import call_gpt
 
+class MultiModalFusion:
+    def analyze(self, data):
+        prompt = f"""
+        Analyze and synthesize insights from the following multi-modal data:
+        {data}
+        Provide a unified summary combining all elements.
+        """
+        return call_gpt(prompt)
