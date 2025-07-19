e5070e8a676b8f38490d34fa0b99154a5d4a4ec3
t81dev <sansdisk0134@icloud.com>
2025-07-19 11:18:50 -0400

    Update external_agent_bridge.py

--- modules/external_agent_bridge.py
+++ modules/external_agent_bridge.py
@@ -1,1 +1,10 @@
+from utils.prompt_utils import call_gpt
 
+class ExternalAgentBridge:
+    def spawn_agent(self, goal):
+        prompt = f"""
+        Simulate spawning a helper agent to accomplish the goal:
+        {goal}
+        What would its plan of action be?
+        """
+        return call_gpt(prompt)
