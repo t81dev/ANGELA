956788d0514211bf77cc7605b4660b70338e69fa
t81dev <sansdisk0134@icloud.com>
2025-07-19 11:18:34 -0400

    Update visualizer.py

--- modules/visualizer.py
+++ modules/visualizer.py
@@ -1,1 +1,11 @@
+from utils.prompt_utils import call_gpt
+
+class Visualizer:
+    def create_diagram(self, concept):
+        prompt = f"""
+        Create a description of a diagram to explain:
+        {concept}
+        Describe how it would look in simple terms.
+        """
+        return call_gpt(prompt)
 
