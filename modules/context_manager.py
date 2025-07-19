52d0cd872d8b9aa23f913197c0a1a6707dea629c
t81dev <sansdisk0134@icloud.com>
2025-07-19 11:16:12 -0400

    Update context_manager.py

--- modules/context_manager.py
+++ modules/context_manager.py
@@ -1,1 +1,9 @@
+class ContextManager:
+    def __init__(self):
+        self.context = {}
 
+    def update_context(self, user_id, data):
+        self.context[user_id] = data
+
+    def get_context(self, user_id):
+        return self.context.get(user_id, "No prior context found.")
